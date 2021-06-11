a3c6a968425f73f8ed709bbf3ae46dcfb3087f79
Ömer Faruk <bayomerfg@gmail.com>
2021-06-11 11:34:11 +0300

    updated
    updated

--- angular/shop/src/app/app.module.ts
+++ angular/shop/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -9,6 +9,11 @@ import { ProductComponent } from './product/product.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
 import {HttpClientModule} from '@angular/common/http';
 import { ProductAddForms1Component } from './product/product-add-forms1/product-add-forms1.component';
+import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
+import { LoginComponent } from './login/login.component';
+import { AccountService } from './services/account.service';
+import { LoginGuard } from './login/login.guard';
+import { AlertifyService } from './services/alertify.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,15 +21,18 @@ import { ProductAddForms1Component } from './product/product-add-forms1/product-
     CategoryComponent,
     ProductComponent,
     ProductFilterPipe,
-    ProductAddForms1Component
+    ProductAddForms1Component,
+    ProductAddForms2Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AccountService,LoginGuard,AlertifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular/shop/src/app/product/product-add-forms2/product-add-forms2.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from 'src/app/category/category';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-add-forms2',
+  templateUrl: './product-add-forms2.component.html',
+  styleUrls: ['./product-add-forms2.component.css'],
+  providers:[CategoryService,ProductService]
+})
+export class ProductAddForms2Component implements OnInit {
+
+  constructor(private formBuilder:FormBuilder,private categoryService:CategoryService,private productService:ProductService,private alertifyService:AlertifyService) { }
+
+   productAddForm!:FormGroup;
+   product:Product = new Product();
+   categories!:Category[];
+   createProductAddForm(){
+     this.productAddForm = this.formBuilder.group({
+       name:["",Validators.required],
+       price:["",Validators.required],
+       description:["",Validators.required],
+       categoryId:["",Validators.required],
+       imageUrl:["",Validators.required]
+     });
+   }
+  ngOnInit(){
+    this.createProductAddForm();
+    this.categoryService.getCategory().subscribe(data=>{this.categories=data});
+  }
+add(){
+  if (this.productAddForm.valid) {
+    this.product = Object.assign({},this.productAddForm.value)
+  }
+  this.productService.addProduct(this.product).subscribe(data=>{this.alertifyService.success(data.name +"added")});
+}
+}
